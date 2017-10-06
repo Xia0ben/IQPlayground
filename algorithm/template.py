da5d8c5bcbe81cb1ba2f15dfeef3b7812bbbf2ae
A.fossart <a.fossart@laposte.net>
2017-10-06 14:23:57 +0200

    Début de la structure du projet

---
+++ algorithm/template.py
@@ -0,0 +1,20 @@
+'''
+Template algorithm file
+
+author :Alexis Fossart
+date :  06/10/2017
+
+Use this template to create your algorithm files
+'''
+
+def example_algorithm(argument1, argument2):
+    '''
+    Document your algorithm here
+    :param argument1: Description de l'argument 1
+    :param argument2: Description de l'arguement 2
+    :return: Description du retour
+    '''
+
+    if (argument1 == argument2):
+        return argument1
+    return argument2
