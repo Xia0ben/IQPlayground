c2679697ed51335ac77e41396dd37883d06ab7ec
A.fossart <a.fossart@laposte.net>
2017-10-06 17:03:36 +0200

    Usage of pickle to save and read an inverted file

--- launch.py
+++ launch.py
@@ -1,10 +1,23 @@
-from files import Reader
+import os.path
+import pickle
 
-file_path = "latimes/la010189"
+from files import Inverted_File, Reader
 
-documents = Reader.read_file(file_path)
+file_path = "latimes/la051090"
+pickle_path = "pickles/la051090"
 
-print("Number of documents read : {}".format(len(documents)))
+if os.path.isfile(pickle_path):
+    with open(pickle_path, "rb") as file:
+        inv_file = pickle.load(file)
+else:
+    documents = Reader.read_file(file_path)
 
-for document in documents:
-    print(document)
+    print("Number of documents read : {}".format(len(documents)))
+
+    inv_file = Inverted_File(documents)
+
+    with open(pickle_path, "wb") as file:
+        pickle.dump(inv_file, file)
+
+for term in inv_file.vocabulary_of_term:
+    print("{} - size : {}".format(term, inv_file.vocabulary_of_term[term]['size']))
