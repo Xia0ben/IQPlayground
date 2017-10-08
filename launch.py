50703408ed72658b1954591d57d2c2955dfddad9
Alexis Fossart <alexis.fossart@insa-lyon.fr>
2017-10-08 14:15:00 +0200

    addition of configuration to chose the algorithm and the number of results

--- launch.py
+++ launch.py
@@ -2,7 +2,19 @@ import os.path
 import pickle
 
 from files import InvertedFile, Reader
-from algorithm import NaiveAlgorithm
+from algorithm import NaiveAlgorithm, SimpleScanAlgorithm
+
+ALGORITHMS = {
+    "NAIVE": NaiveAlgorithm,
+    "SIMPLE": SimpleScanAlgorithm
+}
+
+ALGORITHMS_DESC = {
+    "NAIVE": "A naive Top-K algorithm adding the scores of the documents",
+    "SIMPLE": "A simple algorithm who select the documents where all the queried words appears"
+}
+DEFAULT_NUMBER_OF_RESULTS = 5
+DEFAULT_ALGORITHM = ALGORITHMS["NAIVE"]
 
 file_path = "latimes/la100590"
 pickle_path = file_path.replace("latimes", "pickles")
@@ -27,12 +39,30 @@ else:
 
 print("Loaded Inverted File - {} terms found".format(len(inv_file.vocabulary_of_term)))
 
-algorithm = NaiveAlgorithm()
+algorithm = DEFAULT_ALGORITHM
+number_of_results = DEFAULT_NUMBER_OF_RESULTS
+
+try:
+    in_res = int(input("Number of results desired ? ").strip())
+    number_of_results = in_res
+except ValueError:
+    print("Non-int value entered using default {}".format(DEFAULT_NUMBER_OF_RESULTS))
+
+print("Algorithm description :")
+for (name, desc) in ALGORITHMS_DESC.items():
+    print("{}\t- {}".format(name, desc))
+try:
+    in_alg = ALGORITHMS[input("Choose your algorithm : ").strip().upper()]
+    algorithm = in_alg
+except KeyError:
+    print("Non existing algorithm asked, using default : NAIVE")
+
+algorithm = algorithm()
 
 while True:
     query = input("Query ? ")
 
-    documents = algorithm.execute(query, inv_file, 5)
+    documents = algorithm.execute(query, inv_file, number_of_results)
 
     print("Your query is : {}".format(query))
     if documents is not None:
