b416d64e8c5053789a33c059c61829e08a25628f
Alexis Fossart <alexis.fossart@insa-lyon.fr>
2017-10-08 13:19:36 +0200

    addition of parallel scan for the inverted file
    it is a generator function

--- files/invertedFile.py
+++ files/invertedFile.py
@@ -1,4 +1,7 @@
+import itertools
 from math import log10
+
+import numpy as np
 
 from files import PostingList
 
@@ -55,3 +58,41 @@ class InvertedFile:
             self.vocabulary_of_term[term] = {'size': len(posting_list),
                                              'posting_list': posting_list}
 
+    def parallel_scan(self, terms):
+        '''
+        do the parallel scan of the inverted file
+        is a generator witch returns a doc and its score at each call
+        first find a doc in the current term PL
+        then find the doc in all other terms' PL
+        and then go to the next term
+        if the next doc is already found skip it and go to the next term
+        :param terms: terms used in the scan
+        '''
+
+        found_docs = []
+        ranks = dict()
+        ordonated_accesses = dict()
+        for term in [term for term in terms if term in self.vocabulary_of_term]:
+            ranks[term] = 0
+            # we create a list of generator witch yield their results
+            ordonated_accesses[term] = self.vocabulary_of_term[term]['posting_list'].ordonated_access()
+
+        # while we have a term to explore we continue
+        turn = len(ranks) > 0
+        while turn:
+            # we found the first term with the lowest rank
+            term = next(filter(lambda x: ranks[x] == np.min(list(ranks.values())), ranks.keys()))
+            ranks[term] += 1
+
+            if ranks[term] <= self.vocabulary_of_term[term]['size']:
+                # we do the ordonated access to look for the document
+                item = next(ordonated_accesses[term])
+                if item[0] not in found_docs:
+                    found_docs.append(item[0])
+                    # we found a new doc, we send it
+                    yield item
+                    # for each other terms we send their score for the document
+                    for other_term in [o_term for o_term in terms if o_term is not term]:
+                        yield (item[0], self.vocabulary_of_term[term]['posting_list'].document_score(item[0]))
+            turn = np.any(list(map(lambda w: ranks[w] < self.vocabulary_of_term[w]['size'], ranks.keys())))
+        print('done')
