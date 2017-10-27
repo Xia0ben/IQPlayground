531883175fe9c007be484104f53c3efc1b1633de
bachir <bachir.mourabit.pro@gmail.com>
2017-10-27 15:06:07 +0200

    INVERTED FILE WITH RANDOM INDEXING CODE

--- algorithm/randomIndex.py
+++ algorithm/randomIndex.py
@@ -1,6 +1,7 @@
 # import numpy
 import random
 
+
 class RandomIndex:
 
     n = 100
@@ -22,12 +23,12 @@ class RandomIndex:
     def set_number_ternary(number_ternary):
         RandomIndex.number_ternary = number_ternary
 
+    '''
+    get_random_index_vector() : the result is a context vector
+    '''
+
     @staticmethod
     def get_random_index_vector():
-
-        """
-        print(numpy.random.randint(fin, size=8))
-        """
 
         # Generate random positions :
         positions = random.sample(range(RandomIndex.n), RandomIndex.number_ternary)
@@ -38,25 +39,29 @@ class RandomIndex:
         # Throw positions to there 1 and -1 :
         # print(positions)
 
+        # RandomIndex.number_ternary : the number of apparition for the and -1 in the context vector
         a = [1] * RandomIndex.number_ternary
 
+        # for statement to put 1 in impair position and -1 in pair position for having a mean equal to 0
         for i in range(RandomIndex.number_ternary):
             if i % 2 == 0:
                 a[i] = a[i] * -1
 
         # print(a)
 
+        # generate a vector from 0 to RandomIndex.number_ternary, with size equal to RandomIndex.number_ternary
+        # and the vector contain unique values
         b = random.sample(range(RandomIndex.number_ternary), RandomIndex.number_ternary)
 
         # print(b)
 
+        # initialize the correspondence vector, with maintain the correspondence between 1, -1 and the positions :
         c = [1] * RandomIndex.number_ternary
 
         for i in range(RandomIndex.number_ternary):
             c[b[i]] = a[i]
 
-        # print(c)
-
+        # initialize the result vector with size of n, which contain the 1, -1 in the defined positions
         r = [0] * RandomIndex.n
 
         for i in range(RandomIndex.n):
